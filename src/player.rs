801dd64f2f09d940268775b1f83253bd3beb604b
Alexander Higgins <thealexanderhiggins@gmail.com>
2020-11-21 20:23:11 -0500

    chapter 9

--- src/player.rs
+++ src/player.rs
@@ -3,7 +3,7 @@ use std::cmp::{max, min};
 use rltk::{console, Point, Rltk, VirtualKeyCode};
 use specs::prelude::*;
 
-use crate::{RunState, Viewshed, WantsToMelee};
+use crate::{GameLog, Item, RunState, Viewshed, WantsToMelee, WantsToPickupItem};
 
 use super::{CombatStats, Map, Player, Position, State};
 
@@ -35,6 +35,30 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
             ppos.y = pos.y;
         } else {
             console::log("map blocked!");
+        }
+    }
+}
+
+pub fn get_item(ecs: &mut World) {
+    let player_pos = ecs.fetch::<Point>();
+    let player_entity = ecs.fetch::<Entity>();
+    let entities = ecs.entities();
+    let items = ecs.read_storage::<Item>();
+    let positions = ecs.read_storage::<Position>();
+    let mut gamelog = ecs.fetch_mut::<GameLog>();
+
+    let mut target_item: Option<Entity> = None;
+    for (item_entity, _item, position) in (&entities, &items, &positions).join() {
+        if position.x == player_pos.x && position.y == player_pos.y {
+            target_item = Some(item_entity);
+        }
+    }
+
+    match target_item {
+        None => gamelog.entries.push("There is nothing for you to pick up".to_string()),
+        Some(item) => {
+            let mut pickup = ecs.write_storage::<WantsToPickupItem>();
+            pickup.insert(*player_entity, WantsToPickupItem { collected_by: *player_entity, item }).expect("Unable to insert want to pickup");
         }
     }
 }
@@ -72,6 +96,17 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) -> RunState {
 
             VirtualKeyCode::Numpad1 |
             VirtualKeyCode::B => try_move_player(-1, 1, &mut gs.ecs),
+
+            // Pickup
+            VirtualKeyCode::G => get_item(&mut gs.ecs),
+
+            // Inventory
+            VirtualKeyCode::I => return RunState::ShowInventory,
+
+            // Drop Item
+            VirtualKeyCode::D => return RunState::ShowDropItem,
+
+            // Nothing
             _ => { return RunState::AwaitingInput; }
         },
     }
